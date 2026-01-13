7e58d166b3d489174026e0ab222606a436823fa4
utkarshsharma567 <us80999@gmail.com>
2026-01-13 11:10:08 +0530

    Update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+   baseURL: "https://assessment-backand.onrender.com/api",
   withCredentials: true, // VERY IMPORTANT for cookies
 });
 
